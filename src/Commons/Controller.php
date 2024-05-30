ab980c27b64ba98931e7d7b55e660d7ed070e766
Tuanthanh11 <thanhntph45933@fpt.edu.vn>
2024-05-30 10:45:22 +0700

    first commit

---
+++ src/Commons/Controller.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Thanh\XuongOop\Commons;
+
+class Controller{
+    
+}
---
+++ src/Commons/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Thanh\XuongOop\Commons;
+
+class Model{
+    protected $conn;
+
+    public function __construct()
+    {
+        // THỰC HIỆN KẾT NỐI TỰ ĐỘNG KHI KHỞI TẠO BẤT KỲ
+        //     CLASS NÀO LIÊN QUAN ĐẾN MODEL
+    }
+
+    public function __destruct()
+    {
+        $this->conn = null;
+    }
+}
